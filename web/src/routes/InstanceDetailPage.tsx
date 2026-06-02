99d349a733b0ee4ea0d3ca70d318c07f0f94cddc
carl10086 <yszlx200@163.com>
2026-06-03 01:37:59 +0800

    fix(web): sync contextWindowEnabled in InstanceDetailPage useEffect

--- web/src/routes/InstanceDetailPage.tsx
+++ web/src/routes/InstanceDetailPage.tsx
@@ -37,6 +37,7 @@ export function InstanceDetailPage() {
         apiKey: instance.apiKey,
         opencodeModelId: instance.opencodeModelId,
         kvCacheEnabled: instance.kvCacheEnabled,
+        contextWindowEnabled: instance.contextWindowEnabled,
       });
       setDirty(false);
     }
